2bfa34296a7d7fd0f75a3b72766ca46503f7a6a3
PranavReja <82927686+PranavReja@users.noreply.github.com>
2024-07-23 19:37:00 -0400

    Update socketclient.py

--- RaspberryPi/socketclient.py
+++ RaspberryPi/socketclient.py
@@ -1,1 +1,21 @@
+# client.py
+
+#importing socket module
+import socket
+
+# creating socket instance
+client_object = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+# target ip address and port
+ip_address = '10.0.0.221'
+port = 8000
+
+# instance requesting for connection to the specified address and port
+client_object.connect((ip_address,port))
+
+# receiving response from server
+
+client_input = "face".encode('utf-8')
+        
+client_object.send(client_input)
 
