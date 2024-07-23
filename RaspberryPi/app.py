355b50f70cf68b6a586264ce8bf163f72f3afa25
PranavReja <82927686+PranavReja@users.noreply.github.com>
2024-07-23 19:36:09 -0400

    Update app.py

--- RaspberryPi/app.py
+++ RaspberryPi/app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+import datetime
 
+app = Flask(__name__)
+
+@app.route('/')
+def image():
+	return app.send_static_file("captured_image.jpg")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0',port=8000)
